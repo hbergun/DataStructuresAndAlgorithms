ab03d618ba9d0099168e0c79cee282aa88c0b5fe
Hasan Berk Ergün <hasan.berk.ergun@hotmail.com>
2018-09-05 23:47:18 +0300

    Implement Method

--- TwoWayLinkedList/TwoWayLinkedList/TwoWayLinkedList/Program.cs
+++ TwoWayLinkedList/TwoWayLinkedList/TwoWayLinkedList/Program.cs
@@ -10,6 +10,20 @@ namespace TwoWayLinkedList
     {
         static void Main(string[] args)
         {
+            TwoWayLinkedList DL = new TwoWayLinkedList();
+            DL.AddFirst(5);
+            DL.AddFirst(4);
+            DL.AddFirst(3);
+            DL.AddFirst(2);
+            DL.AddFirst(1);
+            Console.WriteLine(DL.Traverse());
+            Console.WriteLine(DL.GetNode(4).Value);
+            DL.AddPosition(4,0);
+            Console.WriteLine(DL.Traverse());
+            DL.DeletePosition(5);
+            Console.WriteLine(DL.Traverse());
+
+            Console.ReadKey();
         }
     }
 }
--- TwoWayLinkedList/TwoWayLinkedList/TwoWayLinkedList/TwoWayLinkedList.cs
+++ TwoWayLinkedList/TwoWayLinkedList/TwoWayLinkedList/TwoWayLinkedList.cs
@@ -24,40 +24,101 @@ namespace TwoWayLinkedList
                 Size++;
             }
         }
-
         public override void AddLast(int value)
         {
-            
+            Node Temp = new Node { Value = value };
+            GetNode(Size).Next = Temp;
+            Size++;
         }
-
         public override void AddPosition(int position, int value)
         {
-            throw new NotImplementedException();
+            if (position != 0 && position <= Size)
+            {
+                Node Temp = new Node { Value = value };
+                Node GetNodeValue = GetNode(position - 1);
+                Temp.Next = GetNodeValue.Next;
+                Temp.Prev = GetNodeValue;
+                GetNodeValue.Next.Prev = Temp;
+                GetNodeValue.Next = Temp;
+                Size++;
+            }
+            else if (position == 0)
+                AddFirst(value);
+            else
+                return;
+          
         }
 
         public override void DeleteFirst()
         {
-            throw new NotImplementedException();
+            if (Size != 0)
+            {
+                Head = Head.Next;
+                Head.Prev = null;
+            }
+            else
+                return;
+
         }
 
         public override void DeleteLast()
         {
-            throw new NotImplementedException();
+            if (Size != 0)
+            {
+                Node Temp = GetNode(Size - 1);
+                Temp.Next.Prev = null;
+                Temp.Next = null;
+            }
+            else
+                return;
+
         }
 
         public override void DeletePosition(int position)
         {
-            throw new NotImplementedException();
+            if (position > 0 && position < Size)
+            {
+                Node Current = GetNode(position);
+                Current.Prev.Next = Current.Next;
+                Current.Next.Prev = Current.Prev;
+                Current.Prev = null;
+                Current.Next = null;
+            }
+            else if (position == 0)
+                DeleteFirst();
+            else if (position == Size)
+                DeleteLast();
+            else
+                return;
+           
         }
 
         public override Node GetNode(int position)
         {
-            throw new NotImplementedException();
+            if (Size >= position && position >= 0)
+            {
+                Node Temp = Head;
+                for (int i = 0; i < position; i++)
+                    Temp = Temp.Next;
+                return Temp;
+            }
+            else if (position == 0)
+                return Head;
+            else
+                throw new Exception("Geçersiz Pozisyon");
+
         }
 
         public override StringBuilder Traverse()
         {
-            throw new NotImplementedException();
+            StringBuilder ReturnValue = new StringBuilder();
+            Node Temp = Head;
+            while (Temp != null)
+            {
+                ReturnValue.Append(Temp.Value + "-->");
+                Temp = Temp.Next;
+            }
+            return ReturnValue;
         }
     }
 }
